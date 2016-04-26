4aee88aea3f9297cc19c83ba357751e0bd58e478
Wan Xiaolin <macrocosmist@gmail.com>
2016-04-26 22:00:17 +0800

    Update sae.py

--- sae.py
+++ sae.py
@@ -3,7 +3,7 @@
 """
   ------ Sentence Autoencoder ------
   Created by Xiaolin Wan, 4.24.2016
-  Last updated in 4.25.2016
+  Last updated in 4.26.2016
 
   Build a deeply sentence autoencoder with Bi-LSTM.
   We adopt a Bi-LSTM structure with four layer for encoding and four layer for decoding,
@@ -12,31 +12,37 @@
   Draft* Do NOT cite.
 """
 
+import sys
+import time
+
 import numpy
 import theano
 import theano.tensor as tensor
 from theano import config
+from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
 
 class SAE:
     def __init__(self, sae_struct_params, sae_training_params):
         # Set the random number generators' seeds for consistency
-        SEED = 888
-        numpy.random.seed(SEED)
+        self.SEED = 888
+        self.trng = RandomStreams(self.SEED)
+        numpy.random.seed(self.SEED)
 
+        self.nn_weights = {}
         self.struct_params   = sae_struct_params
         self.training_params = sae_training_params
-        self.nn_weights      = self.__init_nn_weights()
+        self.__init_nn_weights()
 
 
     def __numpy_floatX(self, data):
         return numpy.asarray(data, dtype=config.floatX)
 
 
-    def __get_ortho_weights(self, hidden_units_dim):
-        tmp = numpy.random.randn(hidden_units_dim, hidden_units_dim)  # 方阵
+    def __get_ortho_weights(self, dim):
+        tmp = numpy.random.randn(dim, dim)  # 方阵
         ortho_weight = numpy.linalg.svd(tmp)
-        pp = numpy.concatenate([ortho_weight,ortho_weight,ortho_weight,ortho_weight], axis=1)
-        return pp
+        concat_ortho_weight = numpy.concatenate([ortho_weight, ortho_weight, ortho_weight, ortho_weight], axis=1)
+        return concat_ortho_weight
 
 
     def __numpy2theano(self):
@@ -53,20 +59,44 @@ class SAE:
         """
         In this model, Neural network weights includes:
         Encoder:
-            enc_W[i], enc_W[i], enc_W[i], enc_W[i], enc_b[i]
+            enc_W[i], enc_U[i], enc_V[i], enc_b[i]
 
         Decoder:
-            dec_W[i], dec_W[i], dec_W[i], dec_W[i], dec_b[i]
+            dec_W[i], dec_U[i], dec_V[i], dec_b[i]
 
         Feature Extraction:
             mlp_W[i], mlp_b[i]
         """
-        a = {}
-        return a # TODO
+        assert self.nn_weights is None
+
+        enc_ortho_weight = self.__get_ortho_weights(self.struct_params['enc_hidden_dim'])
+        dec_ortho_weight = self.__get_ortho_weights(self.struct_params['dec_hidden_dim'])
+
+        for i in range(0, self.struct_params['enc_n_layers']):
+            self.nn_weights['enc_W'].append(enc_ortho_weight)
+            self.nn_weights['enc_U'].append(enc_ortho_weight)
+            self.nn_weights['enc_V'].append(enc_ortho_weight)
+            self.nn_weights['enc_b'].append(numpy.zeros((4 * self.struct_params['enc_hidden_dim'],)))
+
+        for i in range(0, self.struct_params['enc_n_layers']):
+            self.nn_weights['dec_W'].append(dec_ortho_weight)
+            self.nn_weights['dec_U'].append(dec_ortho_weight)
+            self.nn_weights['dec_V'].append(dec_ortho_weight)
+            self.nn_weights['dec_b'].append(numpy.zeros((4 * self.struct_params['enc_hidden_dim'],)))
 
 
-    def __get_dropout_layer(self):
-        pass # TODO
+
+
+    def __get_dropout_layer(self, state_before, noise):
+        censored = state_before * self.trng.binomial(state_before.shape,p=0.5, n=1, dtype=state_before.dtype)
+        output = tensor.switch(cond = noise,
+                               ift  = censored,
+                               iff  = state_before * 0.5)
+        return output
+
+    def __get_2maxout_layer(self, x):
+        # TODO:
+        return x
 
 
     def encoder_lstm_unit(self, x_, m_, h_, c_, n_layer): # TODO: 根据scan()的行为调整参数顺序
@@ -186,16 +216,26 @@ class SAE:
         """
 
         """
-        # TODO: prehandle data
-        x = []
+        # Used for dropout.
+        use_noise = theano.shared(self.__numpy_floatX(0.))
 
-        # ------ Encoder ------
+        x = tensor.matrix('x', dtype='') # TODO
+        x_mask = tensor.matrix('x_mask', dtype='')
+        y = tensor.matrix('y', dtype='')
+        y_mask = tensor.matrix('y_mask', dtype='')
         forward_encoder, backward_encoder = self.get_encoder
 
-        h_fwec = forward_encoder() #TODO:
-        h_bwec = backward_encoder() #TODO: input need to be reversed
+        n_timesteps = x.shape[0]
+        n_samples   = x.shape[1]
 
-        h_enc = tensor.concatenate([h_fwec, h_bwec], axis=1)
+        # ------ Encoder ------
+        rx = reversed(x)
+        rx_mask = reversed(x_mask)
+
+        h_fenc = forward_encoder(x, x_mask)
+        h_benc = backward_encoder(rx, rx_mask)
+
+        h_enc = tensor.concatenate([h_fenc, h_benc], axis=1)
 
         # ------ Feature Extraction ------
         # We use MLP (Multi Layer Perceptron) to get high-level representation.
@@ -210,6 +250,53 @@ class SAE:
         h_dec = decoder() # TODO:
 
         # ------ 2-Maxout Layer ------
-
+        assert x.shape() % 2 == 0
+        s_2maxout = self.__get_2maxout_layer(h_dec)
 
         # ------ Softmax Layer ------
+        prediction = tensor.nnet.softmax(tensor.dot(s_2maxout, self.nn_weights['softmax_W']) +
+                                         self.nn_weights['softmax_b'])
+
+        # ------ Output ------
+        f_pred_prob = theano.function([x, x_mask], prediction, name='f_pred_prob')
+        f_pred = theano.function([x, x_mask], prediction.argmax(axis=1), name='f_pred')
+
+        off = 1e-8
+        if prediction.dtype == 'float16':
+            off = 1e-6
+
+        cost = -tensor.log(prediction[tensor.arange(n_samples), y] + off).mean()
+
+        return use_noise, x, x_mask, y, f_pred_prob, f_pred, cost
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
