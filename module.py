f877252568325892b6dc05415574dd038bb1e584
Wan Xiaolin <macrocosmist@gmail.com>
2016-05-17 22:38:53 +0800

    Update module.py

--- module.py
+++ module.py
@@ -18,20 +18,24 @@ class Module(object):
     def save(self, filepath):
         container = OrderedDict().fromkeys(self.submodule.keys())
         for name in self.submodule.keys():
-            container[name] = self.submodule[name]
+            container[name] = self.submodule[name].layers2json()
         module_json = json.dumps(container)
         with open(filepath, 'w') as f:
             f.write(module_json)
 
     def load(self, filepath):
         with open(filepath, 'r') as f:
-            module_json = json.load(f.read())
-        assert module_json.keys() == self.submodule.keys
-        for name in module_json.keys():
-            self.submodule[name] = module_json[name]
+            module_json = f.read()
+        container = json.loads(module_json)
+        assert container.keys() == self.submodule.keys()
+        for name in container.keys():
+            self.submodule[name].json2layers(container[name])
 
     def get_params(self):
-        pass
+        module_params = []
+        for kk in self.submodule.keys():
+            module_params.extend(self.submodule[kk].get_params())
+        return module_params
 
 # 下面不加注释把我自己都绕晕了
 
@@ -90,16 +94,14 @@ class DAE(Module):
         self.submodule['sent_enc'] = SentEncoder(self.enc_dim, use_dropout=use_dropout)
         self.submodule['sent_dec'] = SentDecoder(self.dec_dim, use_dropout=use_dropout)
 
-    def get_context_vector(self, x, sents_mask, doc_mask):
-        max_doc_length = tensor.shape(x)[0]
-        # print(type(max_doc_length[0]))
+    def get_context_vector(self, x, sents_mask, doc_mask, max_doc_length):
 
         # x[i]大小为 batch_size * max_sent_length * vocab_size
         # sent_mask[i]大小为 batch_size * max_sent_length
         # 因此需要交换维度batch_size与max_sent_length
 
         s_cv_list = [self.sae.get_context_vector(x[i].dimshuffle(1, 0, 2), sents_mask[i].dimshuffle(1, 0))
-                     for i in range(2)]  # todo
+                     for i in range(max_doc_length)]  # todo
 
         # 对每个i, sae返回的每个context vector都是大小为 batchsize * enc_dim 的二维数组
         # 接下来，用stack()将其合并。得到的三维数组大小为 max_doc_length * batchsize * enc_dim
@@ -108,20 +110,19 @@ class DAE(Module):
         context_vector = self.submodule['sent_enc'].encode(s_cv, doc_mask)
         return context_vector
 
-    def decode(self, context_vector, sents_mask, doc_mask):
+    def decode(self, context_vector, sents_mask, doc_mask, max_doc_length):
         s_cv, _ = self.submodule['sent_dec'].decode(context_vector, doc_mask)
 
         # context_vector大小: batch_size * enc_dim
         # s_cv大小: max_doc_length * batchsize * enc_dim
         # sent_mask大小: max_doc_length * batch_size * max_sent_length
 
-        max_doc_length = s_cv.shape[0]  # TODO
         sents_mask = sents_mask.dimshuffle(0, 2, 1)
         # prob_pred_seq_list = [self.sae.decode(s_cv[0], sents_mask[0]), self.sae.decode(s_cv[1], sents_mask[1])]
         prob_pred_seq, _ = theano.scan(
             fn        = self.sae.decode,
             sequences = [s_cv, sents_mask],
-            n_steps   = 2
+            n_steps   = max_doc_length
         )
 
         # self.submodule['word_dec'].decode(s_cv[i], sents_mask[i])得到的内容为
@@ -134,16 +135,16 @@ class DAE(Module):
         # return s_cv.shape, sents_mask.dimshuffle(0, 2, 1).shape
         return prob_pred_seq[0], prob_pred_seq[1]
 
-    def forward(self, x, sent_mask, doc_mask):
+    def forward(self, x, sent_mask, doc_mask, max_doc_length):
         """
 
         """
-        context_vector = self.get_context_vector(x, sent_mask, doc_mask).T[0:3].T
+        context_vector = self.get_context_vector(x, sent_mask, doc_mask, max_doc_length).T[0:3].T
         # TODO: test 这里一定要注意！！！！！！加入更多的层后删掉！！
 
         # decode()返回的张量pred_seq, prob_pred_seq同尺寸，其大小为：
         #   max_doc_length * max_sents_length * batch_size * vocab_size
         # 使用dimshuffle交换axis后返回与target_seq对应的tensor
         # 将来是否使用dimshuffle取决于train()的输入数据格式
-        pred_seq, prob_pred_seq = self.decode(context_vector, sent_mask, doc_mask)
+        pred_seq, prob_pred_seq = self.decode(context_vector, sent_mask, doc_mask, max_doc_length)
         return pred_seq.dimshuffle(0, 2, 1, 3).shape, prob_pred_seq.dimshuffle(0, 2, 1, 3).shape
