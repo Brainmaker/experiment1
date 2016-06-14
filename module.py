a872634f0ff6d429e3702a7f5959419b2d2ff3ad
Wan Xiaolin <macrocosmist@gmail.com>
2016-06-14 23:10:11 +0800

    Update module.py

--- module.py
+++ module.py
@@ -8,9 +8,14 @@ import theano
 import theano.tensor as tensor
 from theano.tensor.nnet import categorical_crossentropy
 
-from core import EPS, DTYPE, IDX_TYPE
-from submodule import \
-    WordEncoder, SentEncoder, WordDecoder, SentDecoder, MLP
+from core import EPS, DTYPE, USE_2_LAYERS
+
+if USE_2_LAYERS:
+    from submodule_2layers import (
+        WordEncoder, SentEncoder, WordDecoder, SentDecoder, MLP)
+else:
+    from submodule_4layers import (
+        WordEncoder, SentEncoder, WordDecoder, SentDecoder, MLP)
 
 
 class Module(object):
@@ -39,8 +44,6 @@ class Module(object):
             module_params.extend(self.submodule[kk].get_params())
         return module_params
 
-# 下面不加注释把我自己都绕晕了
-
 
 class SAE(Module):
     """
@@ -51,25 +54,25 @@ class SAE(Module):
         mask: 一个包含mini batch中所有句子mask的二维数组。每一列代表一个句子mask，每一行代表一个词mask。
               其大小为 max_timestep * batch_size
     """
-    def __init__(self, vocab_size, enc_dim, dec_dim, use_dropout):
-        #Module.__init__(self, ['word_enc', 'word_dec', 'mlp'])
+    def __init__(self, emb_dim, enc_dim, dec_dim, use_dropout):
         Module.__init__(self, ['word_enc', 'word_dec', 'mlp'])
-        self.vocab_size = vocab_size
+        self.emb_dim = emb_dim
         self.enc_dim = enc_dim
         self.dec_dim = dec_dim
         self.use_dropout = use_dropout
 
-        self.submodule['word_enc'] = WordEncoder(self.enc_dim, self.vocab_size, use_dropout=use_dropout)
+        self.submodule['word_enc'] = WordEncoder(self.enc_dim, self.emb_dim, use_dropout=use_dropout)
         wemb_layer = self.submodule['word_enc'].layers['word_embedding']
-        self.submodule['word_dec'] = WordDecoder(self.dec_dim, self.vocab_size, wemb_layer, use_dropout=use_dropout)
-        self.submodule['mlp'] = MLP(self.enc_dim * 2, self.dec_dim, use_dropout=False)
+        self.submodule['word_dec'] = WordDecoder(self.dec_dim, self.emb_dim, wemb_layer, use_dropout=use_dropout)
+        self.submodule['mlp'] = MLP(self.enc_dim * 2, self.dec_dim, use_dropout=use_dropout)
 
     @staticmethod
-    def _cost(target_seq, prob_pred_seq):
-        prob_pred_seq = tensor.clip(prob_pred_seq, EPS, 1.0 - EPS)
-        cce = categorical_crossentropy(coding_dist=prob_pred_seq, true_dist=target_seq) \
-            .mean(axis=0).mean(axis=0)
-        return cce
+    def _cost(target_seq, pred_seq):
+        pred_seq = tensor.clip(pred_seq, EPS, 1.0 - EPS)
+        # --- cosine similarity ---
+        cs = tensor.sum(target_seq * pred_seq, axis=2) / (target_seq.norm(L=2, axis=2) * pred_seq.norm(L=2, axis=2))
+        cost = cs.mean(axis=0).mean(axis=0)
+        return cost
 
     def get_context_vector(self, x, mask):
         s_emb = self.submodule['word_enc'].word_encode(x, mask)
@@ -77,24 +80,24 @@ class SAE(Module):
         return context_vector
 
     def decode(self, context_vector, mask):
-        pred_seq, prob_pred_seq = self.submodule['word_dec'].decode(context_vector, mask)
-        return pred_seq, prob_pred_seq
+        pred_seq = self.submodule['word_dec'].decode(context_vector, mask)
+        return pred_seq
 
     def forward(self, x, mask):
         context_vector = self.get_context_vector(x, mask)
-        pred_seq, prob_pred_seq = self.decode(context_vector, mask)
-        return pred_seq, prob_pred_seq
+        pred_seq = self.decode(context_vector, mask)
+        return pred_seq
 
     def compile(self, optimizer):
         """
         input_sents: max_sents_length * batch_size * vocab_size
         """
-        input_sents = tensor.tensor3('name', dtype=DTYPE)
+        input_sents = tensor.tensor3('sents', dtype=DTYPE)
         target_sents = input_sents
         mask = tensor.matrix('mask', dtype=DTYPE)
 
-        pred_sents, prob_pred_sents = self.forward(input_sents, mask)
-        cost = self._cost(target_sents, prob_pred_sents)
+        pred_sents = self.forward(input_sents, mask)
+        cost = self._cost(target_sents, pred_sents)
 
         f_updates = theano.function(
             name    = 'f_s_updates',
@@ -158,7 +161,6 @@ class DAE(Module):
         # sent_mask大小: max_doc_length * batch_size * max_sent_length
 
         sents_mask = sents_mask.dimshuffle(0, 2, 1)
-        # prob_pred_seq_list = [self.sae.decode(s_cv[0], sents_mask[0]), self.sae.decode(s_cv[1], sents_mask[1])]
         prob_pred_seq, _ = theano.scan(
             fn        = self.sae.decode,
             sequences = [s_cv, sents_mask],
@@ -169,21 +171,21 @@ class DAE(Module):
         #     pred_seq: max_sents_length * batchsize * vocab_size (one-hot vector)
         #     pred_prob_seq: max_sents_length * batchsize * vocab_size (real value vector)
         # 这里只取pred_prob_seq为列表内容，列表长度为max_doc_length
-
         #prob_pred_seq = tensor.stack(prob_pred_seq_list[1], axis=3)
-
         # return s_cv.shape, sents_mask.dimshuffle(0, 2, 1).shape
+
         return prob_pred_seq[0], prob_pred_seq[1]
 
     def forward(self, x, sent_mask, doc_mask):
         context_vector = self.get_context_vector(x, sent_mask, doc_mask).T[0:3].T
+
         # TODO: test 上面这里一定要注意！！！！！！加入更多的层后删掉！！
         # decode()返回的张量pred_seq, prob_pred_seq同尺寸，其大小为：
         #   max_doc_length * max_sents_length * batch_size * vocab_size
         # 使用dimshuffle交换axis后返回与target_seq对应的tensor
         # 将来是否使用dimshuffle取决于train()的输入数据格式
+
         pred_seq, prob_pred_seq = self.decode(context_vector, sent_mask, doc_mask)
-        # return pred_seq.dimshuffle(0, 2, 1, 3).shape, prob_pred_seq.dimshuffle(0, 2, 1, 3).shape
         return pred_seq.dimshuffle(0, 2, 1, 3), prob_pred_seq.dimshuffle(0, 2, 1, 3)
 
     def compile(self, optimizer):
@@ -196,13 +198,13 @@ class DAE(Module):
         sent_mask = tensor.tensor3('sent_mask', dtype=DTYPE)
         doc_mask = tensor.matrix('doc_mask', dtype=DTYPE)
 
-        pred_docs, prob_pred_docs = self.forward(input_docs, sent_mask, doc_mask)  # TODO
-        costf = self._cost(target_docs, prob_pred_docs)
+        pred_docs = self.forward(input_docs, sent_mask, doc_mask)  # TODO
+        cost = self._cost(target_docs, pred_docs)
 
         f_updates = theano.function(
             name='f_d_updates',
             inputs=[input_docs, sent_mask, doc_mask],
-            outputs=[pred_docs, costf],
+            outputs=[pred_docs, cost],
             updates=optimizer(self.get_params(), costf)
         )
 
