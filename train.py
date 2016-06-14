e3f82a40c0d878c060cdeabb0dcbee6857f763ee
Wan Xiaolin <macrocosmist@gmail.com>
2016-06-14 23:11:02 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,10 +1,11 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import os
+import sys
 import time
 import json
-from collections import defaultdict
+from collections import defaultdict, namedtuple
 
 import numpy
 import theano
@@ -32,26 +33,29 @@ def _shared_zeros_like(x, name=None):
     return theano.shared(dtype_cast(numpy.zeros(x.shape)), name=name)
 
 
-class Optimizer(object):
-    def __init__(self):
-        pass
+def auto_logging(epoch_idx, filename, train_idx, num_of_idx, cost, train_time, log_file):
+    s = 'In epoch %s, file %s:  training index %s in %s, cost=%s, time=%s \n' % \
+                (epoch_idx, filename, train_idx, num_of_idx, cost, train_time)
+    with open(log_file, 'a') as f:
+        f.write(s)
 
-    def optimize(self, params, cost):
-        pass
+
+class Optimizer(object):
+    def __init__(self):pass
+
+    def optimize(self, params, cost):pass
 
 
 class SGD(Optimizer):
-    def __init__(self, lrate, momentum):
+    def __init__(self, lrate):
         Optimizer.__init__(self)
         self.lrate = lrate
-        self.momentum = momentum
 
     def optimize(self, params, cost):
         return [(p, p - self.lrate * g) for p, g in zip(params, tensor.grad(cost=cost, wrt=params))]
 
 
 class AdaDelta(Optimizer):
-    # Not use
     def __init__(self, lrate, rho):
         Optimizer.__init__(self)
         self.lrate = lrate
@@ -74,29 +78,35 @@ class AdaDelta(Optimizer):
 
 
 class Trainer(object):
-    def __init__(self, model, max_epochs, batch_size, lrate, momentum,
-                 dataset_path, vocabs_path, save_path, load_path):
+    def __init__(self, model, max_epochs, batch_size, optimizer,
+                 dataset_path, vocabs_path, word_table_path, save_path, load_path, log_path):
         self.model = model
         self.max_epochs = max_epochs
         self.batch_size = batch_size
-        self.lrate = lrate
-        self.momentum = momentum
+        self.optimizer = optimizer
         self.dataset_path = dataset_path
         self.vocabs_path = vocabs_path
+        self.word_table_path = word_table_path
         self.save_path = save_path
         self.load_path = load_path
-
-        self.optimizer = SGD(self.lrate, self.momentum)
+        self.log_path = log_path
 
         with open(vocabs_path, 'r') as f:
             self.vocabs = defaultdict(lambda : 0, json.loads(f.read()))
 
-        if load_path:
+        with open(word_table_path, 'r') as f:
+            self.word_table = numpy.array(json.loads(f.read()))
+
+        if load_path and os.path.exists(load_path):
             model.load(load_path)
 
+        print('Start compiling...')
+        start_time = time.time()
         self.f_update = model.compile(self.optimizer.optimize)
+        end_time = time.time()
+        print('Compiling finished, compiling time: %s' % (end_time - start_time))
 
-    def to_one_hot(self, x, vocab_size):pass
+    def find_word_vec(self, x, n_samples, maxlen):pass
 
     def prepare_data(self, dataset):pass
 
@@ -124,18 +134,33 @@ class Trainer(object):
             print('In epoch', epoch_idx)
 
             for filename in os.listdir(self.dataset_path):
-                data_file = open(self.dataset_path + '\\' + filename, 'r', encoding='utf-8')
+                data_file = open(self.dataset_path + '/' + filename, 'r', encoding='utf-8')
                 file_dataset = _read_sent_data(data_file, self.vocabs)
+                data_file.close()
+
                 print('Processing file:', filename)
+
                 minibatches_idx = self.get_minibatch_idx(len(file_dataset), shuffle=False)
 
+                num_of_batches_idx = len(minibatches_idx)
+                i = 1
                 for train_idx in minibatches_idx:
+                    start_time = time.time()
+                    print('process training index %s in %s\r' % (i, num_of_batches_idx))
+
                     _, cost = self.get_fupdate_rval(file_dataset, train_idx)
 
                     if numpy.isnan(cost) or numpy.isinf(cost):
                         raise StopIteration
 
-                self.model.save(self.save_path)
+                    end_time = time.time()
+                    time_cost = end_time - start_time
+                    print('the train idx %s process finished\n    time=%s, cost=%s' % (i, time_cost, cost))
+
+                    self.model.save(self.save_path)
+                    auto_logging(epoch_idx, filename, i, num_of_batches_idx, cost, time_cost, self.log_path)
+                    i += 1
+
 
     def train(self):
         start_time = time.time()
@@ -153,49 +178,58 @@ class Trainer(object):
 
 class SAETrainer(Trainer):
     def __init__(self,
-                 vocab_size, s_enc_dim, s_dec_dim, use_dropout,
-                 max_epochs, batch_size, lrate, momentum,
+                 emb_dim, s_enc_dim, s_dec_dim, use_dropout,
+                 max_epochs, batch_size, optimizer,
                  dataset_path='',
                  vocabs_path='',
+                 word_table_path='',
                  save_path='',
-                 load_path=''):
+                 load_path='',
+                 log_path=''):
 
-        sae = SAE(vocab_size, s_enc_dim, s_dec_dim, use_dropout)
+        sae = SAE(emb_dim, s_enc_dim, s_dec_dim, use_dropout)
 
         Trainer.__init__(self,
                          model=sae,
                          max_epochs=max_epochs,
                          batch_size=batch_size,
-                         lrate=lrate,
-                         momentum=momentum,
+                         optimizer = optimizer,
                          dataset_path=dataset_path,
                          vocabs_path=vocabs_path,
+                         word_table_path=word_table_path,
                          save_path=save_path,
-                         load_path=load_path)
+                         load_path=load_path,
+                         log_path=log_path)
 
-    def to_one_hot(self, x, vocab_size):
-        """
-        x: batch_size * max_sents_length
-        return: max_sents_length * batch_size * vocab_size
-        """
-        batch_size, max_sents_length = x.shape
-        onehot_x = numpy.zeros((batch_size, max_sents_length, vocab_size), dtype=IDX_TYPE)
-        for i in range(batch_size):
-            for j in range(max_sents_length):
-                onehot_x[i][j][x[i][j]-1] = 1
-        return onehot_x.swapaxes(1, 0)
+    # def to_one_hot(self, x, vocab_size):
+    #     """
+    #     x: batch_size * max_sents_length
+    #     return: max_sents_length * batch_size * vocab_size
+    #     """
+    #     batch_size, max_sents_length = x.shape
+    #     onehot_x = numpy.zeros((batch_size, max_sents_length, vocab_size), dtype=IDX_TYPE)
+    #     for i in range(batch_size):
+    #         for j in range(max_sents_length):
+    #             onehot_x[i][j][x[i][j]-1] = 1
+    #     return onehot_x.swapaxes(1, 0)
+
+    def find_word_vec(self, x, n_samples, maxlen):
+        emb_dim = self.word_table.shape[1]
+        return self.word_table[x.flatten()].reshape((n_samples, maxlen, emb_dim))
 
     def prepare_data(self, dataset):
         length = [len(s) for s in dataset]
         n_samples = len(dataset)
         maxlen = max(length)
+        emb_dim = self.word_table.shape[1]
 
         x = numpy.zeros((n_samples, maxlen)).astype(IDX_TYPE)
         mask = numpy.zeros((n_samples, maxlen)).astype(DTYPE)
         for idx, val in enumerate(dataset):
             x[idx, :length[idx]] = val
             mask[idx, :length[idx]] = 1
-        return self.to_one_hot(x, self.model.vocab_size), mask.swapaxes(1, 0)
+        x = self.word_table[x.flatten()].reshape((n_samples, maxlen, emb_dim)).astype(DTYPE)
+        return x.swapaxes(1, 0), mask.swapaxes(1, 0)
 
     def get_fupdate_rval(self, dataset, train_idx):
         x, mask = self.prepare_data([dataset[i] for i in train_idx])
@@ -204,15 +238,17 @@ class SAETrainer(Trainer):
 
 class DAETrainer(Trainer):
     def __init__(self,
-                 vocab_size, s_enc_dim, s_dec_dim, d_enc_dim, d_dec_dim, use_dropout,
-                 max_epochs, batch_size, lrate, momentum,
+                 emb_dim, s_enc_dim, s_dec_dim, d_enc_dim, d_dec_dim, use_dropout,
+                 max_epochs, batch_size, optimizer,
                  dataset_path='',
                  vocabs_path='',
+                 word_table_path='',
                  save_path='',
                  load_path='',
+                 log_path='',
                  sae_load_path=''):
 
-        self.sae = SAE(vocab_size, s_enc_dim, s_dec_dim, use_dropout)
+        self.sae = SAE(emb_dim, s_enc_dim, s_dec_dim, use_dropout)
         if sae_load_path:
             self.sae.load(sae_load_path)
 
@@ -222,26 +258,30 @@ class DAETrainer(Trainer):
                          model=dae,
                          max_epochs=max_epochs,
                          batch_size=batch_size,
-                         lrate=lrate,
-                         momentum=momentum,
+                         optimizer = optimizer,
                          dataset_path=dataset_path,
                          vocabs_path=vocabs_path,
+                         word_table_path=word_table_path,
                          save_path=save_path,
-                         load_path=load_path)
+                         load_path=load_path,
+                         log_path=log_path)
 
-    def to_one_hot(self, x, vocab_size):
-        """
-        x 1~vocab_size
-        x: batch_size * max_doc_length * max_sents_length
-        return: max_doc_length * batch_size * max_sents_length * vocab_size
-        """
-        batch_size, max_doc_length, max_sents_length = x.shape
-        onehot_x = numpy.zeros((batch_size, max_doc_length, max_sents_length, vocab_size), dtype=IDX_TYPE)
-        for i in range(batch_size):
-            for j in range(max_doc_length):
-                for k in range(max_sents_length):
-                    onehot_x[i][j][k][x[i][j][k]-1] = 1
-        return onehot_x.swapaxes(1, 0)
+    # def to_one_hot(self, x, vocab_size):
+    #     """
+    #     x 1~vocab_size
+    #     x: batch_size * max_doc_length * max_sents_length
+    #     return: max_doc_length * batch_size * max_sents_length * vocab_size
+    #     """
+    #     batch_size, max_doc_length, max_sents_length = x.shape
+    #     onehot_x = numpy.zeros((batch_size, max_doc_length, max_sents_length, vocab_size), dtype=IDX_TYPE)
+    #     for i in range(batch_size):
+    #         for j in range(max_doc_length):
+    #             for k in range(max_sents_length):
+    #                 onehot_x[i][j][k][x[i][j][k]-1] = 1
+    #     return onehot_x.swapaxes(1, 0)
+
+    def find_word_vec(self, x, emb_dim):
+        pass
 
     @staticmethod
     def _prepare_sentences_data(doc, max_nsents, max_sent_length):
@@ -271,7 +311,7 @@ class DAETrainer(Trainer):
             x[i] = sents_list[i]
             s_mask[i] = st_mask_list[i]
             d_mask[i, :length[i]] = 1
-        return (self.to_one_hot(x, self.model.sae.vocab_size),
+        return (self.find_word_vec(x, self.model.sae.emb_dim),
                 s_mask.swapaxes(1, 0), d_mask.swapaxes(1, 0))
 
     def get_fupdate_rval(self, dataset, train_idx):
