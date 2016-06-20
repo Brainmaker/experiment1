dc1a8bdfc194cc2316022d51762ed785fd340bef
Wan Xiaolin <macrocosmist@gmail.com>
2016-06-20 21:18:51 +0800

    Update core.py

--- core.py
+++ core.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import cmath
 import json
 from collections import OrderedDict
 
@@ -13,8 +14,9 @@ from theano.sandbox.rng_mrg import MRG_RandomStreams
 EPS = 1e-6
 TRNG = MRG_RandomStreams(seed=888)
 theano.config.floatX = 'float32'
-IDX_TYPE = 'int64'
+IDX_TYPE = 'int32'
 DTYPE = theano.config.floatX
+LSTM_OUT_ACT_FUNC = tensor.tanh # tensor.nnet.relu
 USE_2_LAYERS = True
 
 
@@ -54,6 +56,16 @@ def _get_gaussian_weights(dim1, dim2, name=None):
     return theano.shared(w.astype(DTYPE), name=name)
 
 
+def _get_xavier_weights(dim1, dim2, name=None, use_sigmoid=False):
+    _high = cmath.sqrt(6) / cmath.sqrt(dim1 + dim2)
+    _low = -_high
+    if use_sigmoid:
+        w = numpy.random.uniform(low=4 * _low, high=_high, size=(dim1, dim2))
+    else:
+        w = numpy.random.uniform(low=_low, high=_high, size=(dim1, dim2))
+    return theano.shared(w.astype(DTYPE), name=name)
+
+
 def _get_zero_bias(dim, name=None):
     return theano.shared(numpy.zeros((dim,)).astype(DTYPE), name=name)
 
@@ -64,18 +76,20 @@ def _get_4_ortho_weights(dim, name=None):
     return theano.shared(ortho_weight.astype(DTYPE), name=name)
 
 
+def maxout_2(x, x_shape):
+    n_rows = x_shape[0]
+    n_cols = x_shape[1]
+    assert n_cols % 2 == 0
+    maxout = [tensor.zeros((n_rows, 2), dtype=DTYPE)] * (n_cols // 2)
+    for i in range(0, n_cols, 2):
+        maxout[i // 2] = tensor.maximum(x[:, i::n_cols], x[:, (i + 1)::n_cols])
+    return tensor.concatenate(maxout, axis=1)
+
+
 def _slice_4(x, slice_tag, dim):
     if x.ndim == 3:
         return x[:, :, slice_tag * dim: (slice_tag + 1) * dim]
     return x[:, slice_tag * dim: (slice_tag + 1) * dim]
-
-
-class WordTable(object):
-    def __init__(self, word_table_path):
-        self.word_table = json.dumps(word_table_path)
-
-    def forward(self, word_idx):
-        pass
 
 
 class Core(object):
@@ -125,21 +139,28 @@ class WordEncodeLayer(Core):
 
 
 class OutputLayer(Core):
-    def __init__(self, dec_dim, emb_dim):
+    def __init__(self, dec_dim, emb_dim, vocab_size, word_table):
         Core.__init__(self, ['U0', 'E0', 'C0', 'W0'])
         self.dec_dim = dec_dim
         self.emb_dim = emb_dim
-        self.tparams['U0'] = _get_random_weights(self.dec_dim, self.dec_dim, name='U0')
-        self.tparams['E0'] = _get_random_weights(self.emb_dim, self.dec_dim, name='E0')
-        self.tparams['C0'] = _get_random_weights(self.dec_dim, self.dec_dim, name='C0')
-        self.tparams['W0'] = _get_random_weights(self.dec_dim, self.emb_dim, name='W0')
+        self.vocab_size = vocab_size
+        self.word_table = theano.shared(word_table, name='word_table')
+        self.tparams['U0'] = _get_random_weights(self.dec_dim, self.dec_dim * 2, name='U0')
+        self.tparams['E0'] = _get_random_weights(self.emb_dim, self.dec_dim * 2, name='E0')
+        self.tparams['C0'] = _get_random_weights(self.dec_dim, self.dec_dim * 2, name='C0')
+        self.tparams['W0'] = _get_random_weights(self.dec_dim, self.vocab_size, name='W0')
 
     def forward(self, y_tm1, h, context_vector):
-        t = tensor.nnet.relu(tensor.dot(h, self.tparams['U0']) +
-                             tensor.dot(y_tm1, self.tparams['E0']) +
-                             tensor.dot(context_vector, self.tparams['C0']))
-        y = tensor.dot(t, self.tparams['W0'])
-        return y
+        batch_size = context_vector.shape[0]
+        t_bar = (tensor.dot(h, self.tparams['U0']) +
+                 tensor.dot(y_tm1, self.tparams['E0']) +
+                 tensor.dot(context_vector, self.tparams['C0']))
+        t = maxout_2(t_bar, x_shape=(batch_size, self.dec_dim * 2))
+        prob_y = tensor.nnet.softmax(tensor.dot(t, self.tparams['W0'])).astype(DTYPE)
+        #TODO:
+        y_idx = tensor.argmax(prob_y, axis=1)
+        y = self.word_table[y_idx]
+        return y, prob_y
 
 
 class BiLSTMEncodeLayer(Core):
@@ -164,12 +185,12 @@ class BiLSTMEncodeLayer(Core):
         i = tensor.nnet.sigmoid(_slice_4(preact, 0, self.dim))
         f = tensor.nnet.sigmoid(_slice_4(preact, 1, self.dim))
         o = tensor.nnet.sigmoid(_slice_4(preact, 2, self.dim))
-        c = tensor.tanh(_slice_4(preact, 3, self.dim))
+        c = LSTM_OUT_ACT_FUNC(_slice_4(preact, 3, self.dim))
 
         c = f * c_tm1 + i * c
         c = mask[:, None] * c + (1. - mask)[:, None] * c_tm1
 
-        h = o * tensor.tanh(c)
+        h = o * LSTM_OUT_ACT_FUNC(c)
         h = mask[:, None] * h + (1. - mask)[:, None] * h_tm1
 
         return h, c
@@ -181,12 +202,12 @@ class BiLSTMEncodeLayer(Core):
         i = tensor.nnet.sigmoid(_slice_4(preact, 0, self.dim))
         f = tensor.nnet.sigmoid(_slice_4(preact, 1, self.dim))
         o = tensor.nnet.sigmoid(_slice_4(preact, 2, self.dim))
-        c = tensor.tanh(_slice_4(preact, 3, self.dim))
+        c = LSTM_OUT_ACT_FUNC(_slice_4(preact, 3, self.dim))
 
         c = f * c_tm1 + i * c
         c = mask[:, None] * c + (1. - mask)[:, None] * c_tm1
 
-        h = o * tensor.tanh(c)
+        h = o * LSTM_OUT_ACT_FUNC(c)
         h = mask[:, None] * h + (1. - mask)[:, None] * h_tm1
 
         return h, c
@@ -250,11 +271,11 @@ class LSTMDecodeLayer(Core):
         i = tensor.nnet.sigmoid(_slice_4(preact, 0, self.dim))
         f = tensor.nnet.sigmoid(_slice_4(preact, 1, self.dim))
         o = tensor.nnet.sigmoid(_slice_4(preact, 2, self.dim))
-        c = tensor.tanh(_slice_4(preact, 3, self.dim))
+        c = LSTM_OUT_ACT_FUNC(_slice_4(preact, 3, self.dim))
 
         c = f * c_tm1 + i * c
         c = mask[:, None] * c + (1. - mask)[:, None] * c_tm1
-        h = o * tensor.tanh(c)
+        h = o * LSTM_OUT_ACT_FUNC(c)
         h = mask[:, None] * h + (1. - mask)[:, None] * h_tm1
 
         return h, c
